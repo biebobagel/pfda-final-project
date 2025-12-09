64ab19cafc4bcd0a53671f75649b25161836c3c9
biebobagel <dal574479@utdallas.edu>
2025-12-08 20:38:45 -0600

    finished most of main code

--- src/project.py
+++ src/project.py
@@ -1,30 +1,48 @@
 #This is where the main project code will be.
 
 from PIL import Image
-
+from math import sqrt
 from palettes import PALETTES
 
 # Create a sepearate python file containing preset color palettes with RGB color codes.
 
-# Ask the user to upload an image of their choice (preferrably a JPEG). Then store it into a variable.
+def main():
 
-print("Hello! Welcome to the Lighting Transformation Program! Please input the name of your file below that you would like to change. ")
+    # Ask the user to upload an image of their choice (preferrably a JPEG). Then store it into a variable.
 
-filename = input(">>> ")
-img = Image.open(filename)
+    print("Hello! Welcome to the Lighting Transformation Program! Please input the name of your file below that you would like to change. ")
 
-print("Here is your image: ")
-img.show()
-print("")
+    filename = input(">>> ")
+    img = Image.open(filename)
 
-# Prompt the user to select a color palette theme (the options will be listed out for them). Strip and lowercase the response.
+    print("Here is your image: ")
+    img.show()
+    img.close()
+    print("")
 
-print("Here are some color themes you could choose from: type which one you'd like below.")
-for theme in PALETTES:
-    print ("> ", theme, "\n")
-userTheme = input (">>> ".strip.lowercase())
+    # Prompt the user to select a color palette theme (the options will be listed out for them). Strip and lowercase the response.
 
+    print("Here are some color themes you could choose from: type which one you'd like below.")
+    for theme in PALETTES:
+        print ("> ", theme, "\n")
+    userTheme = input(">>> ".strip.lowercase())
 
-# If possible, allow the user to input a color palette of their choice.
+    # Ask the user to select an output image name.
+    
+    print("What would you like the name of your new file to be?")
+    newFilename = input(">>> ")
 
-# Ask the user to select an output image name.
+    # Display the new file.
+
+    newImg = Image.open(newFilename)
+
+    print("Here is your new image!")
+    newImg.show()
+    newImg.close()
+
+    print("Thank you!")
+
+def color_distance():
+
+if __name__ == "__main__":
+    main()
